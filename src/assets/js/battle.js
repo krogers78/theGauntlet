36e8c2d903f7e94f4c35bb4f1d5248a4e529422f
Kloe Rogers <kloerogers@Kloes-MacBook-Pro.local>
2018-03-12 15:13:00 -0400

    Enemies can now be killed and have different chances of attacks

--- src/assets/js/battle.js
+++ src/assets/js/battle.js
@@ -1,4 +1,10 @@
 window.addEventListener('load', () => {
+  // create the enemies
+  const goblin = new Enemy('Goblin', 70, 70, 50);
+  const elf = new Enemy('Elf', 100, 100, 70);
+  const giant = new Enemy('Giant', 200, 200, 125);
+  const rat = new Enemy('Rat', 30, 30, 10);
+
   const playerData = JSON.parse(localStorage.getItem('playerData'));
   const {
     name, className, weapon, armor
@@ -37,20 +43,107 @@ window.addEventListener('load', () => {
     default:
       throw new Error('Cannot create player');
   }
-  const initialLoad = (p) => {
-    console.log('Player', p);
-    // Create a variable for the player article tag
-    const playerHTML = document.querySelector('#playerArticle').children;
-    // Populate the name
-    playerHTML[0].innerHTML = `<i class="fas fa-user mx-1"></i>${p.name}`;
-    // Populate the Current HP
-    playerHTML[1].innerHTML = `<i class="fas fa-heart mx-1"></i>${p.currentHP}`;
-    // Populate the Weapon of Choice
-    playerHTML[2].innerHTML = `<i class="fas fa-fire mx-1"></i>${p.weapon.weaponName}`;
-    // Populate the Armor Amount
-    playerHTML[3].innerHTML = `<i class="fas fa-shield-alt mx-1"></i>${p.defense}`;
-    // Populate the Gold Amount
-    playerHTML[4].innerHTML = `<i class="fas fa-money-bill-alt mx-1"></i>${p.gold}`;
+  // Load the page with the initial player data
+  const initialLoad = (play, gob, el, gi, ra) => {
+    console.log('Player', play);
+    populatePlayerData(document.querySelector('#playerArticle').children, play);
+    // Call the function that decides which enemy to display
+    enemyDecider(play, gob, el, gi, ra);
   };
-  initialLoad(player);
+  // Logic to determine which enemy the player fights
+  const enemyDecider = (play, gob, el, gi, ra) => {
+    // Reset all the enemy HP
+    gob.currentHP = gob.maxHP;
+    el.currentHP = el.maxHP;
+    gi.currentHP = gi.maxHP;
+    ra.currentHP = ra.maxHP;
+    // Generate a random number
+    const randomNumber = Utils.randNum(1, 5);
+    // Create a variable for the enemy article tag
+    const enemyHTML = document.querySelector('#enemyArticle').children;
+    console.log(randomNumber);
+    if (randomNumber === 1) {
+      populateEnemyData(enemyHTML, goblin);
+    } else if (randomNumber === 2) {
+      populateEnemyData(enemyHTML, elf);
+    } else if (randomNumber === 3) {
+      populateEnemyData(enemyHTML, giant);
+    } else {
+      populateEnemyData(enemyHTML, rat);
+    }
+  };
+  // Function to populate the enemy data to be called elsewhere
+  const populatePlayerData = (tag, play) => {
+    // Populate the name
+    tag[0].innerHTML = `<i class="fas fa-user mx-1"></i>${play.name}`;
+    // Populate the Current HP
+    tag[1].innerHTML = `<i class="fas fa-heart mx-1"></i>${play.currentHP}`;
+    // Populate the Weapon of Choice
+    tag[2].innerHTML = `<i class="fas fa-fire mx-1"></i>${play.weapon.weaponName}`;
+    // Populate the Armor Amount
+    tag[3].innerHTML = `<i class="fas fa-shield-alt mx-1"></i>${play.defense}`;
+    // Populate the Gold Amount
+    tag[4].innerHTML = `<i class="fas fa-money-bill-alt mx-1"></i>${play.gold}`;
+  };
+  // Function to populate the enemy data to be called elsewhere
+  const populateEnemyData = (tag, enemy) => {
+    tag[0].innerHTML = `<i class="fas fa-user mx-1"></i>${enemy.name}`;
+    tag[1].innerHTML = `<i class="fas fa-heart mx-1"></i>${enemy.currentHP}`;
+    localStorage.setItem('currentEnemy', enemy.name);
+  };
+  // Function to determine which enemy is being fought
+  const attackEnemy = (play, gob, el, gi, ra) => {
+    // Grab the curent enemy from localStorage
+    const currentEnemy = localStorage.getItem('currentEnemy');
+    if (currentEnemy.toLowerCase() === 'goblin') {
+      // Attack function to determine the amount of damage
+      attackAmount(play, gob);
+      // Update the enemy info
+      populateEnemyData(document.querySelector('#enemyArticle').children, gob);
+    } else if (currentEnemy.toLowerCase() === 'elf') {
+      // Attack function to determine the amount of damage
+      attackAmount(play, el);
+      // Update the enemy info
+      populateEnemyData(document.querySelector('#enemyArticle').children, el);
+    } else if (currentEnemy.toLowerCase() === 'giant') {
+      // Attack function to determine the amount of damage
+      attackAmount(play, gi);
+      // Update the enemy info
+      populateEnemyData(document.querySelector('#enemyArticle').children, gi);
+    } else if (currentEnemy.toLowerCase() === 'rat') {
+      // Attack function to determine the amount of damage
+      attackAmount(play, ra);
+      // Update the enemy info
+      populateEnemyData(document.querySelector('#enemyArticle').children, ra);
+    }
+    // Check if the enemy has died and call the enemy decider function
+    gob.currentHP <= 0 ? enemyDecider(play, gob, el, gi, ra) : null;
+    el.currentHP <= 0 ? enemyDecider(play, gob, el, gi, ra) : null;
+    gi.currentHP <= 0 ? enemyDecider(play, gob, el, gi, ra) : null;
+    ra.currentHP <= 0 ? enemyDecider(play, gob, el, gi, ra) : null;
+    console.log(currentEnemy);
+  };
+  const attackAmount = (play, enemy) => {
+    // Random number and DOM element
+    const randNum = Utils.randNum(0, 15);
+    const domElement = document.querySelector('#atkMessage');
+    // Update player armor and currentHP
+    populatePlayerData(document.querySelector('#playerArticle').children, play);
+
+    if (randNum === 2) {
+      domElement.innerHTML = `You missed and the ${enemy.name} attacked.`;
+      play.decreaseHP(enemy.weapon);
+    } else if (randNum === 3) {
+      domElement.innerHTML = 'CRITICAL HIT';
+      enemy.decreaseHP(play.weapon.damage * Utils.randNum(1, 5));
+    } else {
+      domElement.innerHTML = 'The attack was successful';
+      enemy.decreaseHP(play.weapon.damage);
+    }
+  };
+  initialLoad(player, goblin, elf, giant, rat);
+
+  // Button Events
+  // Attack
+  document.querySelector('#atkBtn').addEventListener('click', () => attackEnemy(player, goblin, elf, giant, rat));
 });
