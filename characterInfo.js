8682716b8bf2185675d6777d671d4589bb6bbf8f
Kloe Rogers <kloerogers@Kloes-MacBook-Pro.local>
2018-03-11 20:09:04 -0400

    Re-adding project files

---
+++ characterInfo.js
@@ -0,0 +1,6 @@
+var info = {
+	"playerName":"default",
+	"className":"mage",
+	"nameWeapon":"fire",
+	"armorRating":0,
+}
--- src/controllers/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-module.exports = app => app.use('/', router);
-
-router.get('/', (req, res, next) => {
-  res.send('Hello World');
-})
